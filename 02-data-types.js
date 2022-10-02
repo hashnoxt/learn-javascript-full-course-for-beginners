91cf09c4410527fcae2b0b7c3fafadd56bb539b6
hashan nalaka <hashannalaka0123@gmail.com>
2022-10-02 20:06:52 +0530

    Initial commit
    IN-01

---
+++ 02-data-types.js
@@ -0,0 +1,7 @@
+/* undefined, null, boolean, string, symbol,number, object */
+
+var myName = "Beau"; // through whole programme
+
+let ourName = "FreeCodeCamp"; // only within scope
+
+const pi = 3.14 // constant
